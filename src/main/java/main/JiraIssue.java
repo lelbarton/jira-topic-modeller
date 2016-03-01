435c85c19e91c3864314daab3b719d52c8ec920d
lelbarton <lauraelbarton@gmail.com>
2016-03-01 13:29:48 -0800

    Parse JSON response and create new Issue objects

--- src/main/java/main/JiraIssue.java
+++ src/main/java/main/JiraIssue.java
@@ -2,12 +2,22 @@ package main;
 
 public class JiraIssue implements Issue {
 
-	private int id;
 	private String key;
+	private int id;
+	private String date;
+	private String summary;
 	private String contentText;
-	private String author;
+	private String authorName;
+	private String userName;
 
-	public JiraIssue() {
-
+	public JiraIssue(String key, int id, String summary, String desc, String date, String displayName,
+			String userName) {
+		this.id = id;
+		this.key = key;
+		this.date = date;
+		this.summary = summary;
+		this.contentText = desc;
+		this.authorName = displayName;
+		this.userName = userName;
 	}
 }
--- src/main/java/retrieve/JiraJSONParser.java
+++ src/main/java/retrieve/JiraJSONParser.java
@@ -1,8 +1,12 @@
 package retrieve;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+
+import main.Issue;
+import main.JiraIssue;
 
 public class JiraJSONParser {
 
@@ -13,7 +17,42 @@ public class JiraJSONParser {
 
 		// JSONObject response = (JSONObject) obj.get("response");
 		JSONArray issues = (JSONArray) obj.get("issues");
-		System.out.println(issues.getJSONObject(0));
+
+		for (int i = 0; i < issues.length(); i++) {
+			JSONObject issue = issues.getJSONObject(i);
+			try {
+				// get issue key and id number
+				String key = issue.getString("key");
+				String idString = issue.getString("id");
+				int id = Integer.parseInt(idString);
+
+				// get date, summary, and main text
+				JSONObject fields = issue.getJSONObject("fields");
+				String summary = fields.getString("summary");
+
+				String desc = fields.getString("description");
+
+				String date = fields.getString("created");
+
+				// get author info
+				JSONObject creatorInfo = fields.getJSONObject("creator");
+				String displayName = creatorInfo.getString("displayName");
+				String userName = creatorInfo.getString("name");
+
+				// create new issue using the extracted data
+				Issue newIssue = new JiraIssue(key, id, summary, desc, date, displayName, userName);
+				// System.out.println(key);
+
+				// TODO: put issues into some sort of collection
+
+			} catch (JSONException jse) {
+				System.out.println(jse.getStackTrace());
+			}
+		}
+
+		// JSONObject sample = issues.getJSONObject(0);
+		// System.out.println(sample.getJSONObject("fields"));
+		// System.out.println(issues.getJSONObject(0));
 	}
 }
 //
